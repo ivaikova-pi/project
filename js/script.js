0f168d541ca1a08f48a2935d618b9e292b0cceed
ivaikova-pi <poroxinalida@gmail.com>
2026-05-19 13:32:44 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-'use strict'
-document.addEventListener("DOMContentLoaded", function() {
-    console.log('Скрипт отработал корректно')
+const welcomeButtonModal = document.querySelector(".header-button");
+const modalApplication = document.querySelector(".registration-forms");
+if (welcomeButtonModal && modalApplication) {
+    if (welcomeButtonModal && modalApplication) {
+        welcomeButtonModal.addEventListener("click", function (event) {
+            modalApplication.removeAttribute("hidden");
+        });
+    }
+}
+
+window.addEventListener("click", function(event) {
+    if (event.target === modalApplication) {
+        modalApplication.setAttribute("hidden", true);
+    }
 });
